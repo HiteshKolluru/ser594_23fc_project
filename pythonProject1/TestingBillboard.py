b5c41d64ff518e57f8c67b68cb28dbf3163faba9
HiteshKolluru-asu <hkolluru@asu.edu>
2024-10-20 18:02:08 -0700

    Found data
    Scraped billboard site to make a list of songs
    and then used genuine to gather details of the songs like title of song, artist and lyrics. had issues with Genre so will try figure that out later.

---
+++ pythonProject1/TestingBillboard.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.billboard.com/charts/hot-100/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+filename = "billboard_hot_100.csv"
+
+headers = "Song, Artist, Last Week, Peak Position, Weeks on Chart\n"
+
+print("Starting the Web Scraper to scrape" + url + "for the weeks top 100 songs")
+
+# Dictionary to store data
+list_song_100 = {
+    "Song": [],
+    "Artist": [],
+    "Last_week": [],
+    "Peak_position" : [],
+    "WeeksonChart": [],
+}
+
+#Scraping for each of the 100 songs
+for i, container in enumerate(soup.select("ul.o-chart-results-list-row")):
+    song = container.find("h3", {"class": "c-title"}).text.strip()
+    artist = container.find("span", {"class": "a-no-trucate"}).text.strip()
+
+    last_week, peak_position, weeks_on_chart = container.find_all(
+        "ul", {"class": "lrv-a-unstyle-list"})[-1].text.strip().split()
+
+    # Printing data being scraped
+    print("\nPosition: "+ str(i+1))
+    print("Song: " + song)
+    print("Artist: " + artist)
+    print("Last Week: " + last_week)
+    print("Peak Position: " + peak_position)
+    print("Weeks on Chart: " + weeks_on_chart)
+
+    #storing data scraped in dictionary
+    list_song_100['Song'].append(song)
+    list_song_100['Artist'].append(artist)
+    list_song_100['Last_week'].append(last_week)
+    list_song_100['Peak_position'].append(peak_position)
+    list_song_100['WeeksonChart'].append(weeks_on_chart)
+
+# saving dictionary in filename
+df = pd.DataFrame(list_song_100)
+df.to_csv(filename)
---
+++ pythonProject1/collectingData.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+
+import lyricsgenius
+import pandas as pd
+from lyricsgenius import Genius
+
+token = "mKu246Cbo3UL41cYIsjLLD3WN7dl5tjyYYQvIIZ5kI028e_3JuzY_jN9HRpvePi7"
+genius = lyricsgenius.Genius(token, timeout = 500, remove_section_headers = True, verbose = False)
+
+import csv
+# field_names= ['artist', 'lyrics']
+song_dict = {
+    'artist': [],
+    'song_id': [],
+    'title': [],
+    'url' : [],
+    'lyrics': [],
+}
+
+with open('billboard_hot_100.csv', mode ='r')as file:
+  csvFile = csv.reader(file)
+  for lines in csvFile:
+    print(lines)
+    song = genius.search_song(lines[0], artist=lines[1])
+    if song != None:
+        # print(song.lyrics)
+        song_dict['artist'].append(song.artist)
+        song_dict['title'].append(song.title)
+        song_dict['url'].append(song.url)
+        song_dict['song_id'].append(song.id)
+        song_dict['lyrics'].append(song.lyrics)
+
+
+page = 1
+tags = ['pop', 'rock', 'country']
+for tag in tags:
+    res = genius.tag(tag)
+    for hit in res['hits']:
+        # song_lyrics = genius.lyrics(song_url=hit['url'])
+        if song != None:
+            # print(song.lyrics)
+            song = genius.search_song(hit['title'])
+            song_dict['artist'].append(song.artist)
+            song_dict['title'].append(song.title)
+            song_dict['url'].append(song.url)
+            song_dict['song_id'].append(song.id)
+            song_dict['lyrics'].append(song.lyrics)
+
+
+df = pd.DataFrame(song_dict)
+df.to_csv('ArtistwithLyrics.csv')
